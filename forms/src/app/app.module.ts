567f18c64177baf93a61edddc412e09f27e4da6f
Priscila Lima <prsclalim@gmail.com>
2020-03-05 10:09:50 -0300

    Controles ngForm, ngSubmit e ngModel

--- forms/src/app/app.module.ts
+++ forms/src/app/app.module.ts
@@ -1,6 +1,7 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
+import { FormsModule } from '@angular/forms';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { TemplateFormComponent } from './template-form/template-form.component';
@@ -14,6 +15,7 @@ import { DataFormComponent } from './data-form/data-form.component';
   ],
   imports: [
     BrowserModule,
+    FormsModule,
     AppRoutingModule
   ],
   providers: [],
