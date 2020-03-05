30d3e07108a86144c0875527095f94df370d59b6
Priscila Lima <prsclalim@gmail.com>
2020-03-05 08:58:21 -0300

    Criação do projeto e instalação do bootstrap 3

---
+++ forms/src/app/app.module.ts
@@ -0,0 +1,22 @@
+import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
+
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { TemplateFormComponent } from './template-form/template-form.component';
+import { DataFormComponent } from './data-form/data-form.component';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    TemplateFormComponent,
+    DataFormComponent
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
