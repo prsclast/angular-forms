987f886e4fd77d7f16b5645085da510386f1e755
Priscila Lima <prsclalim@gmail.com>
2020-03-11 11:59:41 -0300

    Submetendo valores com HTTP POST

--- forms/src/app/template-form/template-form.component.ts
+++ forms/src/app/template-form/template-form.component.ts
@@ -17,6 +17,10 @@ export class TemplateFormComponent implements OnInit {
     console.log(form);
 
     // console.log(this.usuario);
+
+    this.http.post('https://httpbin.org/post', JSON.stringify(form.value))
+      .pipe(map(res => res))
+      .subscribe(dados => console.log(dados));
   }
 
   constructor(private http: HttpClient) { }
