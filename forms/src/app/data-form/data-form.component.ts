df148e526f7f6c7b4fb81484eb3b421fafc400e6
Priscila Lima <prsclalim@gmail.com>
2020-03-16 10:58:47 -0300

    Endereço (Migrando de template driven para data driven)

--- forms/src/app/data-form/data-form.component.ts
+++ forms/src/app/data-form/data-form.component.ts
@@ -27,7 +27,14 @@ export class DataFormComponent implements OnInit {
 
     this.formulario = this.formBuilder.group({
       nome: [null, [Validators.required, Validators.minLength(3), Validators.maxLength(20)]],
-      email: [null, [Validators.required, Validators.email]]
+      email: [null, [Validators.required, Validators.email]],
+      cep: [null, Validators.required],
+      numero: [null, Validators.required],
+      complemento: [null],
+      rua: [null, Validators.required],
+      bairro: [null, Validators.required],
+      cidade: [null, Validators.required],
+      estado: [null, Validators.required]
     });
   }
 
@@ -62,7 +69,7 @@ export class DataFormComponent implements OnInit {
     }
   }
 
-  aplicaCSSErro(campo) {
+  aplicaCSSErro(campo: string) {
     return {
       'has-error': this.verificaValidTouched(campo),
       'has-feedback': this.verificaValidTouched(campo)
